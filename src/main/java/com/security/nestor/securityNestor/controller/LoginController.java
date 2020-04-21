fad106fe2b791711227cc5a59854e65226b0df86
Néstor <nestor.amichetti@gmail.com>
2020-04-21 08:12:11 -0300

    Agregado generador de passwords

--- src/main/java/com/security/nestor/securityNestor/controller/LoginController.java
+++ src/main/java/com/security/nestor/securityNestor/controller/LoginController.java
@@ -1,13 +1,21 @@
 package com.security.nestor.securityNestor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login() {
+	public String login(@RequestParam(name="error")String error, Model model,RedirectAttributes flash) {
+		
+		if(error!=null) {
+			flash.addFlashAttribute("danger", "Usuario o contraseña incorrecto/s");
+		}
+		
 		return "login";
 	}
 }
