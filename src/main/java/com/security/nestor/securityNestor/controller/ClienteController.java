7950cd56221480ee4cfd362234d0ec4a40210a92
Néstor <nestor.amichetti@gmail.com>
2020-04-22 10:26:15 -0300

    Agregado mensajes con thymeleaf

--- src/main/java/com/security/nestor/securityNestor/controller/ClienteController.java
+++ src/main/java/com/security/nestor/securityNestor/controller/ClienteController.java
@@ -2,10 +2,14 @@ package com.security.nestor.securityNestor.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.security.nestor.securityNestor.entity.Cliente;
 import com.security.nestor.securityNestor.service.IClienteService;
@@ -27,8 +31,22 @@ public class ClienteController {
 		return "index";
 	}
 	
-	@GetMapping("/alta")
-	public String alta(Model model) {
+	@GetMapping("/form")
+	public String formulario(Model model) {
+		Cliente cliente = new Cliente();
+		model.addAttribute("titulo","Formulario cliente");
+		model.addAttribute("cliente",cliente);
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String guardar(@Valid Cliente cliente, BindingResult result,Model model) {
+		
+		if(result.hasErrors()){
+			model.addAttribute("titulo","Formulario cliente");
+			return "form";
+		}
+		
 		return "index";
 	}
 	
